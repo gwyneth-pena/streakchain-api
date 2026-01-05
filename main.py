c9f29538ca1377d9b49ddd070fb011d609b76742
Gwen <gwen@Gwens-MacBook-Air.local>
2026-01-05 16:03:07 +0800

    Changed allowed origin

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200"],
+    allow_origins=["http://localhost:4200", "https://streakchain.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
