f8d4e131defb71d004f4872baeeec8aadb102df4
Gwen <gwen@Gwens-MacBook-Air.local>
2026-02-02 13:17:25 +0800

    Expose content disposition

--- utils/shared.py
+++ utils/shared.py
@@ -76,5 +76,5 @@ def generate_xlsx(data: list, filename: str):
     return StreamingResponse(
         output,
         media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-        headers={"Content-Disposition": f"attachment; filename={filename}.xlsx"}
+        headers={"Content-Disposition": f'attachment; filename="{filename}.xlsx"'}
     )
