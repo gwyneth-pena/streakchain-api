53bca22256ccc11218a0561f73c9169fdd111936
Gwen <gwen@Gwens-MacBook-Air.local>
2026-07-26 13:31:51 +0800

    Get all habits with max streak

--- services/statistics.py
+++ services/statistics.py
@@ -14,7 +14,8 @@ def get_statistics(user_id: int, start_date: date, end_date: date, db: Session):
 
     # Tracking variables
     max_streak = 0
-    max_streak_habit = None
+    max_streak_habits = []
+    init_max_streak_habits = []
     total_logs = 0
     total_frequency = 0
     totals_per_habit = {}
@@ -42,14 +43,22 @@ def get_statistics(user_id: int, start_date: date, end_date: date, db: Session):
         
         if streaks > max_streak:
             max_streak = streaks
-            max_streak_habit.append(habit.name)
 
+        if streaks == max_streak and streaks > 0:
+            init_max_streak_habits.append({
+                "habit_name": habit.name,
+                "streaks": streaks
+            })
+            
+        
+
+    max_streak_habits = [habit["habit_name"] for habit in init_max_streak_habits if habit["streaks"] == max_streak]
     # Total completion percentage calculation
     total_percentage = round((total_logs / total_frequency) * 100, 2) if total_frequency > 0 else 0.0
 
     return {
         "habit_with_max_streak": {
-            "habit_with_max_streak": max_streak_habit.join(', ') if max_streak_habit else None,
+            "habit_with_max_streak": ', '.join(max_streak_habits) if max_streak_habits else None,
             "max_streak": max_streak
         },
         "total_logs": total_logs,
